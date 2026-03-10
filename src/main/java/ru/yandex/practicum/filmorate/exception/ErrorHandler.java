fa854bf9c63fab9fecbd87bf92659c6ee31ea2ca
Igor Tumanov <ihortum@yandex.ru>
2026-03-10 20:10:46 +0300

    add: Exception

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
@@ -0,0 +1,33 @@
+package ru.yandex.practicum.filmorate.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@Slf4j
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationException(final ValidationException e) {
+        log.warn("Ошибка валидации: {}", e.getMessage());
+        return Map.of("error", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFoundException(final NotFoundException e) {
+        log.warn("Объект не найден: {}", e.getMessage());
+        return Map.of("error", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleThrowable(final Throwable e) {
+        log.error("Внутренняя ошибка сервера", e);
+        return Map.of("error", "Произошла внутренняя ошибка сервера");
+    }
+}
