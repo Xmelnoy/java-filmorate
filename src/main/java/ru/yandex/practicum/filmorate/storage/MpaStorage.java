07044e799c296524a5eb16e997df565740b157db
Igor Tumanov <ihortum@yandex.ru>
2026-04-03 12:18:13 +0300

    add: DAO

--- src/main/java/ru/yandex/practicum/filmorate/storage/MpaStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/MpaStorage.java
@@ -1,10 +1,9 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.Mpa;
-
 import java.util.List;
 
 public interface MpaStorage {
-    List<Mpa> getAllMpa();
     Mpa getMpaById(Integer id);
+    List<Mpa> getAllMpa();
 }
